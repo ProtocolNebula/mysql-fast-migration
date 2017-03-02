e884472c48b7022e9c73dfdd94c589c2fd42136d
ProtocolNebula <powergm@hotmail.es>
2017-03-02 01:16:44 +0100

    Fix backup host var

--- scripts/backup.php
+++ scripts/backup.php
@@ -1,11 +1,11 @@
 <?php
 //die();
 include_once 'config.php';
-include_once dirname(__FILE__) . 'Mysqldump.php';
+include_once dirname(__FILE__) . '/Mysqldump.php';
 
 echo 'Starting backup of ',$dbname,'...<hr />';
 
-$dump = new Ifsnop\Mysqldump\Mysqldump('mysql:host='.dbhost.';dbname='.$dbname, $dbuser, $dbpass);
+$dump = new Ifsnop\Mysqldump\Mysqldump('mysql:host='.$dbhost.';dbname='.$dbname, $dbuser, $dbpass);
     $dump->start('dump.sql');
 	
 echo 'Dump database sucessfully!<br /><br />',
